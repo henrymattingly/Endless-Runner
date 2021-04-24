fb066352cfcc5aa18bfdace7ebb9d18128604137
Henry Mattingly <hsmattin@ucsc.edu>
2021-04-24 01:05:51 -0700

    jhvjhgbhjb

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,17 +15,18 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0,640,480,'background').setOrigin(0,0);
 
          //create bag
-        this.Bag = new Bag(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'bag').setOrigin(0.5, 0);
+        this.bag = new Bag(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'bag').setOrigin(0.5, 0);
+        //this.bag = this.physics.add.image(400,300, 'bag').setOrigin(0.5);
+
+        this.garbage = this.physics.add.image(200,100, 'garbage');
+        
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
-        //create the trash asset
-        //this.garbage = new Garbage(this,game.config.width + borderUISize * 3, borderUISize*5 + borderPadding * 2, 'garbage', 0, 20).setOrigin(0, 0);
-        //this.garbage = new Garbage(this, game.config.width, borderUISize*6 + borderPadding*4, 'garbage', 0, 10).setOrigin(0,0);
-
+        
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -34,13 +35,11 @@ class Play extends Phaser.Scene {
     }
 
     update(){
-        
-        //allows bag to move
-        this.Bag.update();
-        //this.garbage.update();
-
         //move background
         this.background.tilePositionX -= 1;
+
+        //allows bag to move
+        this.bag.update();
 
     }
 
